87bf1714722d82b0bdc973c5410848fc191431c7
tchief <cyber.tchief@gmail.com>
2013-12-17 23:31:30 +0200

    Set definitions. Implemented Clean target.

--- build.fsx
+++ build.fsx
@@ -1,14 +1,60 @@
 #r @"tools\FAKE\tools\FakeLib.dll"
 
+open System.IO
+open System.Linq
 open Fake
 
 // --------------------------------------------------------------------------------------
 // Definitions
 
+let binProjectName = "Orc.Sort"
+let testProjectName = "Orc.Sort.Tests"
+let netVersion = "NET40"
+
+let srcDir  = @".\src\"
+let deploymentDir  = @".\deployment\"
+let packagesDir = deploymentDir @@ "packages"
+let nugetPath = srcDir @@ @"\.nuget\nuget.exe"
+
+let outputDir = @".\output\"
+let outputReleaseDir = outputDir @@ "release" @@ netVersion
+let outputBinDir = outputReleaseDir @@ binProjectName
+let outputTestDir = outputReleaseDir @@ testProjectName
+let testResultsDir = outputDir @@ "TestResults"
+
+let ignoreBinFiles = "*.vshost.exe"
+let ignoreBinFilesPattern = @"\**\" @@ ignoreBinFiles
+let outputBinFiles = !! (outputBinDir @@ @"\**\*.*")
+                            -- ignoreBinFilesPattern
+
+let tests = srcDir @@ @"\**\*.Test.csproj" 
+let allProjects = srcDir @@ @"\**\*.csproj" 
+
+let testProjects  = !! tests
+let otherProjects = !! allProjects
+                        -- tests
 
 // --------------------------------------------------------------------------------------
 // Clean build results
 
+Target "CleanPackagesDirectory" (fun _ ->
+    CleanDirs [packagesDir]
+)
+
+Target "DeleteOutputFiles" (fun _ ->
+    !! (outputBinDir + @"\**\*.*")
+        ++ (outputTestDir + @"\**\*.*")
+        -- ignoreBinFilesPattern
+    |> DeleteFiles
+)
+
+Target "DeleteOutputDirectories" (fun _ ->
+    CreateDir outputDir
+    DirectoryInfo(outputDir).GetDirectories("*", SearchOption.AllDirectories)
+    |> Array.filter (fun d -> not (d.GetFiles(ignoreBinFiles, SearchOption.AllDirectories).Any()))
+    |> Array.map (fun d -> d.FullName)
+    |> DeleteDirs
+)
 
 // --------------------------------------------------------------------------------------
 // Build projects
@@ -26,6 +72,7 @@ open Fake
 // Combined targets
 
 Target "Clean" DoNothing
+"CleanPackagesDirectory" ==> "DeleteOutputFiles" ==> "DeleteOutputDirectories" ==> "Clean"
 
 Target "Build" DoNothing
 
