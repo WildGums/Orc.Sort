79dc8f826f45a1601debe2a794711acd1bfc08e8
orcomp <orcomp@server>
2014-07-25 12:02:18 +1000

    (+) Added EnumerateFrom() method
    (*) Update version

---
+++ src/Orc.Sort.Tests/Extensions/EnumerateFromTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Orc.Sort.Extensions;
+
+namespace Orc.Sort.Tests.Extensions
+{
+    [TestFixture]
+    public class EnumerateFrom
+    {
+        [Test]
+        public void EnumerateFrom_UniqueValues_StartFrom_A()
+        {
+            var collection = new List<string>() {"A", "B", "C", "D", "E", "F", "G" };
+
+            var result = collection.EnumerateFrom("A");
+
+            var expected = new List<string>() { "A", "B", "C", "D", "E", "F", "G" };
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void EnumerateFrom_UniqueValues_StartFrom_C()
+        {
+            var collection = new List<string>() { "A", "B", "C", "D", "E", "F", "G" };
+
+            var result = collection.EnumerateFrom("C");
+
+            var expected = new List<string>() { "C", "D", "E", "F", "G", "A", "B"};
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void EnumerateFrom_UniqueValues_StartFrom_C_NonCyclical()
+        {
+            var collection = new List<string>() { "A", "B", "C", "D", "E", "F", "G" };
+
+            var result = collection.EnumerateFrom("C", false);
+
+            var expected = new List<string>() { "C", "D", "E", "F", "G"};
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void EnumerateFrom_UniqueValues_StartFrom_G_NonCyclical()
+        {
+            var collection = new List<string>() { "A", "B", "C", "D", "E", "F", "G" };
+
+            var result = collection.EnumerateFrom("G", false);
+
+            var expected = new List<string>() {"G" };
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void EnumerateFrom_NonUniqueValues_StartFrom_A_ThrowException()
+        {
+            var collection = new List<string>() { "A", "B", "C", "A", "E", "F", "G" };
+
+            Assert.Throws<ArgumentException>(() => collection.EnumerateFrom("A").ToArray());
+        }
+
+        [Test]
+        public void EnumerateFrom_StartValueDoeNotExist_StartFrom_X_ThrowException()
+        {
+            var collection = new List<string>() { "A", "B", "C", "A", "E", "F", "G" };
+
+            Assert.Throws<ArgumentException>(() => collection.EnumerateFrom("X").ToArray());
+        }
+    }
+}
